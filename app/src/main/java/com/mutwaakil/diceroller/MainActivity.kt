d8cb92fd69fe0b6d9858b1fd7deb818503e4762a
mutwakilmo <mutwakilmo@gmail.com>
2020-07-20 16:28:42 +0200

    Finding Views Efficiently

--- app/src/main/java/com/mutwaakil/diceroller/MainActivity.kt
+++ app/src/main/java/com/mutwaakil/diceroller/MainActivity.kt
@@ -10,15 +10,21 @@ import kotlin.random.Random
 import kotlin.random.Random.Default.nextInt
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
             rollDice()
         }
+        diceImage = findViewById(R.id.dice_image)
     }
 
     private fun rollDice() {
@@ -34,7 +40,6 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         diceImage.setImageResource(drawableResource)
 
 
